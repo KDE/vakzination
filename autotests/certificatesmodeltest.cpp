013ac37926fd20b02c76c8993ba0c05dfab1cc89
Nicolas Fella <nicolas.fella@gmx.de>
2021-08-06 01:11:59 +0200

    Add autotest for certificatemodel

---
+++ autotests/certificatesmodeltest.cpp
@@ -0,0 +1,73 @@
+// SPDX-FileCopyrightText: 2021 Nicolas Fella <nicolas.fella@gmx.de>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "certificatesmodel.h"
+
+#include <QSignalSpy>
+#include <QTest>
+
+#include <KLocalizedString>
+
+class CertificatesModelTest : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void testEmptyModel()
+    {
+        QStandardPaths::setTestModeEnabled(true);
+
+        CertificatesModel model(false);
+
+        QCOMPARE(model.rowCount({}), 0);
+    }
+
+    void testBuiltinCerts()
+    {
+        QStandardPaths::setTestModeEnabled(true);
+
+        CertificatesModel model(true);
+
+        QCOMPARE(model.rowCount({}), 4);
+
+        QCOMPARE(model.data(model.index(0), CertificatesModel::TypeRole), KHealthCertificate::Vaccination);
+    }
+
+    void testImport()
+    {
+        QStandardPaths::setTestModeEnabled(true);
+
+        CertificatesModel model(true);
+
+        const QUrl testFile = QUrl::fromLocalFile(QFINDTESTDATA("negative-pcr-test-fr.pdf"));
+
+        QSignalSpy errorSpy(&model, &CertificatesModel::importError);
+        model.importCertificate(testFile);
+
+        QCOMPARE(errorSpy.count(), 0);
+
+        QCOMPARE(model.rowCount({}), 5);
+
+        QCOMPARE(model.data(model.index(4), CertificatesModel::TypeRole), KHealthCertificate::Test);
+    }
+
+    void testNonexistantImport()
+    {
+        QStandardPaths::setTestModeEnabled(true);
+
+        CertificatesModel model(true);
+
+        const QUrl testFile = QStringLiteral("/does/not/exist");
+
+        QSignalSpy errorSpy(&model, &CertificatesModel::importError);
+        model.importCertificate(testFile);
+
+        QCOMPARE(errorSpy.count(), 1);
+        QCOMPARE(errorSpy.first().first().toString(), i18n("Could not open file: "));
+
+        QCOMPARE(model.rowCount({}), 4);
+    }
+};
+
+QTEST_MAIN(CertificatesModelTest)
+
+#include "certificatesmodeltest.moc"
